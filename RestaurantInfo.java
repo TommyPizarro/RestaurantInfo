a456e4a7fa63278d74350bf9f122461134a67d53
TommyP <tommypizarro6@gmail.com>
2025-12-24 23:08:39 -0700

    zip code variable and format output
    Updated variable name for zip code and improved output formatting.

--- RestaurantInfo.java
+++ RestaurantInfo.java
@@ -1,16 +1,19 @@
 public class RestaurantInfo {
+
     public static void main(String[] args) {
-        
+.
         String restaurantName = "Wings Pier";
         String streetAddress = "7008 W 10th St";
         String city = "Greeley";
         String state = "CO";
-        String zipCode = "80634";
+        String zip = "80634"; //updated to zip for easier reading
 
-        System.out.println("Restaurant Name: " + restaurantName);
-        System.out.println("Business Address: " + streetAddress);
-        System.out.println("City, State, Zip Code: " + city + ", " + state + " " + zipCode);
+        //Grouping outout into one fom=rmated block to improve strcture
+        System.out.println("Restaurant Information:");
+        System.out.println("-----------------------");
+
+        System.out.println("Name: " + restaurantName);
+        System.out.println("Address: " + streetAddress);
+        System.out.println("Location: " + city + ", " + state + " " + zip);
     }
-
-
-
+}
